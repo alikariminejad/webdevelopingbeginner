f29c88d13ec8ca8225a512517f79213a7a364825
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2023-09-04 14:10:08 +0330

    jQuery

---
+++ 10-jQuery/1-myscript.js
@@ -0,0 +1,45 @@
+// $('h1').click(function(){
+//     console.log('There was a click!')
+// })
+
+// $('li').click(function( ){
+//     console.log('any li was clicked')
+// })
+
+// $('h1').click(function(){
+//     $(this).text("I was changed!")
+// })
+
+//key press
+// $('input').eq(0).keypress(function(){
+//     $('h3').toggleClass('turnBlue');
+// })
+
+// $('input').eq(0).keypress(function(event){
+//     console.log(event);
+// })
+
+// $('input').eq(0).keypress(function(event){
+//     if (event.which === 13){
+//         $('h3').toggleClass('turnBlue')
+//     }
+// })
+
+
+// on()
+// $('h1').on('dblclick',function(){
+//     $(this).toggleClass('turnBlue');
+// })
+
+// $('h1').on('mouseenter',function(){
+//     $(this).toggleClass('turnBlue');
+// })
+
+// animation
+// $('input').eq(1).on('click',function(){
+//     $('.container').fadeOut(3000)
+// })
+
+$('input').eq(1).on('click',function(){
+    $('.container').slideUp(3000)
+})
