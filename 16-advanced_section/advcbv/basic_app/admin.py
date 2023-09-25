e280520d087cedf85b9b004035c6ba6ed8234919
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2023-09-25 23:14:55 +0330

    advanced Section

--- 16-advanced_section/advcbv/basic_app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from basic_app.models import School, Student
-
-admin.site.register(School)
-admin.site.register(Student)
--- 16-advanced_section/advcbv/basic_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path, include
-from basic_app import views
-
-app_name = 'basic_app'
-
-urlpatterns = [
-    re_path(r'^$',views.SchoolDetailView.as_view(),name='list'),
-    path('school/<int:pk>/', views.SchoolDetailView.as_view(), name='school-detail'),
-]
